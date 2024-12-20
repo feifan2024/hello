34a51bb8c668b969e95850100489799e841773df
feifanbaba2024 <port.1986@163.com>
2024-12-20 15:40:11 +0800

    11

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title('我的  第一个 Streamlit  应用')
-    st.write('Hello, World!')
+    st.title('我的 Streamlit  应用')
+    st.write('Hello')
 
 if __name__ == '__main__':
     main()
